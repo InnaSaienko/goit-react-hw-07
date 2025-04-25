604bfb715ba796776bcaca09b02da4cc313cd6b0
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-25 09:16:12 +0200

    add extra reducers for each fetch accordingly

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,20 +1,42 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {addContact, deleteContact, fetchContacts} from "./contactsOps.js";
 
 const initialState = {
     items: [],
+    loading: false,
+    error: null,
+};
+
+const handelPending = state => {
+    state.isLoading = true;
+};
+
+const handelFulfilled = (state, action) => {
+    state.isLoading = false;
+    state.error = null;
+    state.items = action.payload;
+};
+
+const handelRejected = (state, action) => {
+    state.isLoading = false;
+    state.error = action.payload;
 };
 
 const slice = createSlice({
     name: "contacts",
     initialState,
-    reducers: {
-        addContact: (state, action) => {
-            state.items.push(action.payload);
-        },
-        deleteContact: (state, action) => {
-            state.items = state.items.filter(item => item.id !== action.payload)
-        },
-    }
-})
-export const {addContact, deleteContact} = slice.actions;
+    extraReducers: builder => {
+        builder
+            .addCase(fetchContacts.pending, handelPending)
+            .addCase(fetchContacts.fulfilled, handelFulfilled)
+            .addCase(fetchContacts.rejected, handelRejected)
+            .addCase(addContact.pending, handelPending)
+            .addCase(addContact.fulfilled, handelFulfilled)
+            .addCase(addContact.rejected, handelRejected)
+            .addCase(deleteContact.pending, handelPending)
+            .addCase(deleteContact.fulfilled, handelFulfilled)
+            .addCase(deleteContact.rejected, handelRejected)
+    },
+});
+
 export default slice.reducer;
