b46beb49bddc5b6c0dd13404bce915de2930b2e3
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-07 08:39:30 +0200

    remove to unwanted side effects

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,17 @@ import ContactForm from "./components/ContactForm/ContactForm.jsx";
 import SearchBox from "./components/SearchBox/SearchBox.jsx";
 import ContactList from "./components/ContactList/ContactList.jsx";
 import Loader from "./components/Loader/Loader.jsx";
-import {selectContacts, selectError, selectLoading} from "./redux/contactsSlice.js";
+import {selectError, selectLoading} from "./redux/contactsSlice.js";
 
 const App = () => {
     const dispatch = useDispatch();
     const isLoading = useSelector(selectLoading);
     const isError = useSelector(selectError);
 
-    const contacts = useSelector(selectContacts);
-
     useEffect(() => {
-        if (contacts.length === 0) {
             dispatch(fetchContacts());
-        }
+    }, [dispatch]);
 
-    }, [dispatch, contacts]);
     return (
         <div className={s.main}>
             <ContactForm/>
@@ -28,7 +24,6 @@ const App = () => {
             {isLoading && !isError && < Loader/>}
             <ContactList/>
         </div>
-
     )
 }
 
